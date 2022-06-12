ed0fad39f98f76a2c1371352bdbbeb72d4a7d638
Megasouner <megasounder@gmail.com>
2022-06-12 15:19:21 +0300

    hw-8-in progress

---
+++ 8 HW/main.py
@@ -0,0 +1,11 @@
+class Human:
+    def __init__(self, gender, age, country, city):
+        self.gender = gender
+        self.age = age
+        self.country = country
+        self.city = city
+
+
+
+if __name__ == '__main__':
+ run
